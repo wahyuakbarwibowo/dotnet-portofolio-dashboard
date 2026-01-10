92177ffbcd650b81eb19a0625974aa408cdec7f2
Wahyu Akbar Wibowo <wahyuakbar.work@gmail.com>
2026-01-10 20:07:49 +0700

    initial project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dotnet_portofolio_dashboard.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
